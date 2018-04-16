6352e5cea8770920922c347890f5778cc3877ed7
yankang <yankang@xiaoantimes.com>
2018-04-16 18:20:03 +0800

    layout

--- src/views/page/layout/layout.vue
+++ src/views/page/layout/layout.vue
@@ -1,9 +1,19 @@
 <template>
-  <router-view></router-view>
+  <div class="app-wrapper">
+    <side-bar></side-bar>
+    <div class="main-container">
+      <nav-bar></nav-bar>
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
 <style>
